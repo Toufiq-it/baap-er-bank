8c7f08f049a1dd1d28c07e3d95999c7363c1eec3
toufiq-it <ittoufikit@gmail.com>
2023-09-29 15:09:35 +0600

    first commit

---
+++ Bap er bank/js/login.js
@@ -0,0 +1,21 @@
+// step 1 : add click event handler with the submit button
+document.getElementById('btn-submit').addEventListener('click',function(){
+    // step-2 : get the email address inside the email input filed
+    // note: always rememder to use .value to get text from an input filed
+    const emailFiled = document.getElementById('user-email');
+    const email = emailFiled.value;
+    
+    // step-3 : get password inside the password input filed
+    const passwordFiled = document.getElementById('user-password');
+    const password = passwordFiled.value;
+    
+    // DANGER : DO NOT VERIFY email password on the client side
+    // step-4 : verify email & password
+    if (email === 'santo@gmail.com' && password === 'gopon') {
+        console.log('valid user');
+    }else{
+        console.log('invalid user');
+    }
+
+
+})
